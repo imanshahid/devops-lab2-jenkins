a3520fd474d8eab087cff19a0c0a94052b1a393c
iron(i)man <148297143+imanshahid@users.noreply.github.com>
2026-09-24 23:15:37 +0500

    Update greeting message in Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, DevOps pipeline!");
+        System.out.println("Hello, DevOps pipeline!- i changed the code a bitt");
     }
 }
